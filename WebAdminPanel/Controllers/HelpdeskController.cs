246e300cfa7a3b4c6572c30dfb74ff2afa9bbdf0
KamilMammadov <kamiltm@code.edu.az>
2022-11-12 23:03:13 +0400

    Helpdesk Controller and view created

---
+++ WebAdminPanel/Controllers/HelpdeskController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class HelpdeskController : Controller
+    {
+        public IActionResult Ticket()
+        {
+            return View();
+        }
+        public IActionResult Reports()
+        {
+            return View();
+        }
+        public IActionResult Agents()
+        {
+            return View();
+        }
+    }
+}

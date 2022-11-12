0df9eeae2572ff8e1b97ac7c47d60e5ab61e9c38
KamilMammadov <kamiltm@code.edu.az>
2022-11-12 20:07:17 +0400

    Ecommerce ProductList written

--- WebAdminPanel/Controllers/EcommerceController.cs
+++ WebAdminPanel/Controllers/EcommerceController.cs
@@ -8,5 +8,9 @@ namespace WebAdminPanel.Controllers
         {
             return View();
         }
+        public IActionResult ProductList()
+        {
+            return View();
+        }
     }
 }

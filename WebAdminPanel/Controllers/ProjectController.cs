5a7787ee0dd572aba3f6ee9e934ff8319f92339c
KamilMammadov <kamiltm@code.edu.az>
2022-11-12 19:58:52 +0400

    Project Controller written

---
+++ WebAdminPanel/Controllers/ProjectController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public IActionResult Clients()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+        public IActionResult Project()
+        {
+            return View();
+        }
+
+        public IActionResult Task()
+        {
+            return View();
+        }
+
+        public IActionResult KanbanBoard()
+        {
+            return View();
+        }
+        public IActionResult Chat()
+        {
+            return View();
+        }
+        public IActionResult Users()
+        {
+            return View();
+        }
+        public IActionResult ProjectCreate()
+        {
+            return View();
+        }
+    }
+}

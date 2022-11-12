4c1c0869ad13c5ee2aae37b7a5ef30a3e209f68f
KamilMammadov <kamiltm@code.edu.az>
2022-11-13 00:06:29 +0400

    General Controller and views created

---
+++ WebAdminPanel/Controllers/GeneralController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class GeneralController : Controller
+    {
+        public IActionResult Allotments()
+        {
+            return View();
+        }
+        public IActionResult Expenses()
+        {
+            return View();
+        }
+        public IActionResult Departments()
+        {
+            return View();
+        }
+        public IActionResult Insurance()
+        {
+            return View();
+        }
+
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult Leaves()
+        {
+            return View();
+        }
+
+        public IActionResult Holidays()
+        {
+            return View();
+        }
+
+        public IActionResult Attendance()
+        {
+            return View();
+        }
+
+        public IActionResult Chat()
+        {
+            return View();
+        }
+
+    }
+}

4657f81b7c8d61841e531ddcdbd439f76e4faac3
KamilMammadov <kamiltm@code.edu.az>
2022-11-12 18:45:17 +0400

    AdminPanels index page configured

---
+++ WebAdminPanel/Controllers/Analytic.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+	public class Analytic : Controller
+	{
+		public IActionResult Customers()
+		{
+			return View();
+		}
+	}
+}

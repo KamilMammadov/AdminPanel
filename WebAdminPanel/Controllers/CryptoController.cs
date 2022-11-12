d6a8ac87bfc5f8ad0d068b007e790111843f3753
KamilMammadov <kamiltm@code.edu.az>
2022-11-12 19:56:19 +0400

    Crypto Controller and view

---
+++ WebAdminPanel/Controllers/CryptoController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class CryptoController : Controller
+    {
+        public IActionResult Exchange()
+        {
+            return View();
+        }
+
+        public IActionResult Wallet()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult Ico()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}

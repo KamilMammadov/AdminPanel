cc6a44efa4660f4cf7fad596a111f281ed7b4c97
KamilMammadov <kamiltm@code.edu.az>
2022-11-12 23:45:21 +0400

    Payments and Staff controllers and views written

---
+++ WebAdminPanel/Controllers/PaymentsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        public IActionResult AllPayments()
+        {
+            return View();
+        }
+        public IActionResult Invoice()
+        {
+            return View();
+        }
+        public IActionResult Cashless()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ WebAdminPanel/Controllers/StaffController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult AllStaff()
+        {
+            return View();
+        }
+        public IActionResult AddMember()
+        {
+            return View();
+        }
+        public IActionResult EditMember()
+        {
+            return View();
+        }
+        public IActionResult MemberProfile()
+        {
+            return View();
+        }
+        public IActionResult Salary()
+        {
+            return View();
+        }
+    }
+}

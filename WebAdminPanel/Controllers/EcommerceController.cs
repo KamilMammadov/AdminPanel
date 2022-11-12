c147f18fae8230c6c03dae4c9df42fe57eab6bed
KamilMammadov <kamiltm@code.edu.az>
2022-11-12 20:00:01 +0400

    Ecommerce Controller and view written

---
+++ WebAdminPanel/Controllers/EcommerceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class EcommerceController : Controller
+    {
+        public IActionResult Products()
+        {
+            return View();
+        }
+    }
+}

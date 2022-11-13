1899154095d101a936470aaa44538118b8b91a6b
KamilMammadov <kamiltm@code.edu.az>
2022-11-13 12:46:53 +0400

    links are attached

---
+++ WebAdminPanel/Controllers/EmailController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAdminPanel.Controllers
+{
+    public class EmailController : Controller
+    {
+        public IActionResult Inbox()
+        {
+            return View();
+        }
+        public IActionResult ReadEmail()
+        {
+            return View();
+        }
+    }
+}

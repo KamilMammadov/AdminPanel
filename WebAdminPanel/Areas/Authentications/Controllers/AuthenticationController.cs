891f8488065e257f3f3922305096c176245455d2
KamilMammadov <kamiltm@code.edu.az>
2022-11-13 17:22:04 +0400

    Authentication controller finished

--- WebAdminPanel/Areas/Authentications/Controllers/AuthenticationController.cs
+++ WebAdminPanel/Areas/Authentications/Controllers/AuthenticationController.cs
@@ -17,5 +17,41 @@ namespace WebAdminPanel.Areas.Authentications.Controllers
         {
             return View();
         }
+        public IActionResult RegisterAlt()
+        {
+            return View();
+        }
+        public IActionResult RePassword()
+        {
+            return View();
+        }
+        public IActionResult RePasswordAlt()
+        {
+            return View();
+        }
+        public IActionResult LockScreen()
+        {
+            return View();
+        }
+        public IActionResult LockScreenAlt()
+        {
+            return View();
+        }
+        public IActionResult Error404()
+        {
+            return View();
+        }
+        public IActionResult Error404Alt()
+        {
+            return View();
+        }
+        public IActionResult Error500()
+        {
+            return View();
+        }
+        public IActionResult Error500Alt()
+        {
+            return View();
+        }
     }
 }
